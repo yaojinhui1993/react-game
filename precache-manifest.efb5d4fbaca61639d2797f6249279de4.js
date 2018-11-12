b2f1b5d9481eb195723a6cea9121ac8c9501649e
yaojinhui <yaojinhui1993@gmail.com>
2018-11-12 21:13:50 +0800

    Updates

---
+++ precache-manifest.efb5d4fbaca61639d2797f6249279de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda9c7c5162c773fb401",
+    "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
+  },
+  {
+    "revision": "c09dcbf66a072fe98cd4",
+    "url": "/react-game/static/js/main.c09dcbf6.chunk.js"
+  },
+  {
+    "revision": "8649b2a3c146a3d374b2",
+    "url": "/react-game/static/js/1.8649b2a3.chunk.js"
+  },
+  {
+    "revision": "c09dcbf66a072fe98cd4",
+    "url": "/react-game/static/css/main.f3daca95.chunk.css"
+  },
+  {
+    "revision": "a3caaac1b20ff24e2f4725ac00fab40c",
+    "url": "/react-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1a0876fa703344dce697b3bc2c0c0d3.js"
+  "/react-game/precache-manifest.efb5d4fbaca61639d2797f6249279de4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
